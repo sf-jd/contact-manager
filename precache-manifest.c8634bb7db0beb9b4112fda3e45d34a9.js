ddbfac467832201e5bffcfbdacbd551033ae794b
Santiago <sfl.code@gmail.com>
2019-03-10 11:53:55 -0300

    Updates

---
+++ precache-manifest.c8634bb7db0beb9b4112fda3e45d34a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "7364cd71bec18e6302f0",
+    "url": "/contact-manager/static/js/main.7364cd71.chunk.js"
+  },
+  {
+    "revision": "d81f2ff304a6a3f0a6fa",
+    "url": "/contact-manager/static/js/1.d81f2ff3.chunk.js"
+  },
+  {
+    "revision": "7364cd71bec18e6302f0",
+    "url": "/contact-manager/static/css/main.4e3153b1.chunk.css"
+  },
+  {
+    "revision": "d81f2ff304a6a3f0a6fa",
+    "url": "/contact-manager/static/css/1.2170a589.chunk.css"
+  },
+  {
+    "revision": "7893759df15ede30b9d50a38ac7982ce",
+    "url": "/contact-manager/index.html"
+  }
+];
