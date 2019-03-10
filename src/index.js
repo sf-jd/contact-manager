9e631cf74ec5bcb6b9879c14ccbbd14f8a2893ed
Santiago Fernandez <sfl.code@gmail.com>
2019-03-10 11:42:53 -0300

    GH pages
    add github pages and update  routing

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import AddContact from "./components/AddContact";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "./components/Context";
 
 import { faPlus, faSortDown, faTimes } from "@fortawesome/free-solid-svg-icons";
